87ee0ed02daf42618c923f4231932a7cc97c20b4
Risda Tamam <risda123tamam@gmail.com>
2021-09-09 18:59:54 +0700

    Initial commit

---
+++ src/main/kotlin/Car.kt
@@ -0,0 +1,17 @@
+class Car(name: String, types: String, color: String, tires: Int) {
+    var carName = name
+    var typeCar = types
+    var carColor = color
+    var numberOfTires = tires
+
+    fun result(): String {
+        val _result =
+                "------ Deskripsi Mobil ------" +
+                "\nNama Mobil: " + carName +
+                "\nType Mobil: " + typeCar +
+                "\nWarna Mobil: " + carColor +
+                "\nJumlah Ban: " + numberOfTires + "\n" +
+                "-----------------------------"
+        return _result
+    }
+}
---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,5 @@
+fun main() {
+    val mobil = Car("Mazda", "Sport","Blue", 4)
+    val hasil = mobil.result()
+    println(hasil)
+}
